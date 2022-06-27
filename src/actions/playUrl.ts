3208d39e011b73282abbe1c558e8d91c7669546a
Maxwell Rosenberg <max@maxrosenb.com>
2022-06-27 17:21:28 -0400

    add sound effects

---
+++ src/actions/playUrl.ts
@@ -0,0 +1,67 @@
+import Discord from "discord.js";
+import ytdl from "ytdl-core";
+import { Song, ServerInfo, YtdlResults } from "../types";
+import { playThroughDiscord } from "../utils/utils";
+import { skip } from "./skip";
+import { joinVoiceChannel } from "@discordjs/voice";
+import { togglePause } from "./pause";
+const youtubesearchapi = require("youtube-search-api");
+
+export const playUrl = async (
+  message: Discord.Message,
+  serverInfo: ServerInfo | undefined,
+  queue: Map<string, ServerInfo>,
+  url: string,
+  assertDominance: boolean = false
+): Promise<void> => {
+  if (serverInfo?.isPaused) {
+    serverInfo.isPaused = false;
+    togglePause(serverInfo, message, false);
+    return;
+  }
+  const someEmoji = message.guild?.emojis?.cache.find(
+    (emoji) => emoji.name === "2434pepebusiness"
+  );
+  message.channel.send(`${someEmoji}`);
+  if (
+    !message.client.user ||
+    !message.guild ||
+    !message.member?.voice.channel ||
+    message.member?.voice.channel.permissionsFor(message.client.user) === null
+  )
+    return;
+
+  const songInfo: ytdl.videoInfo = await ytdl.getInfo(
+    url // BOING sound
+  );
+
+  const song: Song = {
+    title: songInfo.videoDetails.title,
+    url: songInfo.videoDetails.video_url,
+    userAddedBy: message.author.username,
+  };
+
+  if (!serverInfo) {
+    const connection = joinVoiceChannel({
+      channelId: message.member.voice.channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+      selfDeaf: false,
+    });
+    // If we've never seen this server before, add it to the Map
+    const serverConstruct: ServerInfo = {
+      textChannel: message.channel,
+      connection: connection,
+      songs: [song],
+      volume: 5,
+      isPaused: false,
+    };
+
+    queue.set(message.guild?.id, serverConstruct);
+    playThroughDiscord(message.guild, serverConstruct.songs[0], queue);
+    return;
+  }
+
+  serverInfo.songs.splice(1, 0, song);
+  return skip(message, serverInfo);
+};
--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ import { playPlaylist } from "./actions/playPlaylist";
 import { reset } from "./actions/reset";
 import { currentlyPlaying } from "./actions/currentPlaying";
 import { togglePause } from "./actions/pause";
+import { playUrl } from "./actions/playUrl";
 try {
   if (!TOKEN) {
     console.log("No token found. Please set TOKEN in config.ts");
@@ -119,6 +120,49 @@ try {
     // PAUSE COMMAND
     if (message.content.startsWith(`${PREFIX}pause`)) {
       return togglePause(serverInfo, message);
+    }
+
+    // PLAYBOING COMMAND
+    if (message.content.startsWith(`${PREFIX}boing`)) {
+      return playUrl(
+        message,
+        serverInfo,
+        queue,
+        "https://www.youtube.com/watch?v=d7vfbyFl5kc"
+      );
+    }
+
+    // GROCERY COMMAND
+    if (message.content.startsWith(`${PREFIX}grocery`)) {
+      return playUrl(
+        message,
+        serverInfo,
+        queue,
+        "https://www.youtube.com/watch?v=GTsBU3RtF2c&t=766s"
+      );
+    }
+
+    // SCOOBY COMMAND
+    if (
+      message.content.startsWith(`${PREFIX}scoob`) ||
+      message.content.startsWith(`${PREFIX}scooby`)
+    ) {
+      return playUrl(
+        message,
+        serverInfo,
+        queue,
+        "https://www.youtube.com/watch?v=xW6UWCUMhNE"
+      );
+    }
+
+    // PART COMMAND
+    if (message.content.startsWith(`${PREFIX}party`)) {
+      return playUrl(
+        message,
+        serverInfo,
+        queue,
+        "https://www.youtube.com/watch?v=N4Db0oYKXvw"
+      );
     }
 
     message.channel.send("You need to enter a valid command!");
