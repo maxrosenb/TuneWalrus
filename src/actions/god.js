216501c93e20e35ae396195577879618e436adc0
Maxwell Rosenberg <max@maxrosenb.com>
2022-06-27 21:34:25 -0400

    remove js files

--- src/actions/god.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.god = void 0;
-var god = function (message) {
-    message.channel.send("TuneWalrus is love. TuneWalrus is life. Accept TuneWalrus into yours and live happily for the rest of your days.");
-    return;
-};
-exports.god = god;
