14c8417480c9fe9d8bc388e4f86490a3394b663d
Maxwell Rosenberg <max@maxrosenb.com>
2022-06-27 20:11:43 -0400

    fix stop command, add death command

--- src/actions/play.ts
+++ src/actions/play.ts
@@ -95,6 +95,10 @@ export const play = async (
 
   serverInfo.songs.push(song);
 
+  if (serverInfo.songs.length) {
+    playThroughDiscord(message.guild, serverInfo.songs[0], queue);
+  }
+
   message.channel.send(`${song.title} has been added to the queue!`);
   return;
 };
--- src/actions/stop.ts
+++ src/actions/stop.ts
@@ -1,10 +1,13 @@
 import Discord from "discord.js";
 import { ServerInfo } from "../types";
 import { player } from "../utils/utils";
+import { reset } from "./reset";
 
 export const stop = (
   message: Discord.Message,
-  serverInfo: ServerInfo | undefined
+  serverInfo: ServerInfo | undefined,
+  queue: Map<string, ServerInfo>,
+  client: Discord.Client
 ): void => {
   if (!serverInfo?.connection) {
     return;
@@ -24,4 +27,5 @@ export const stop = (
   serverInfo.songs = [];
   player.stop();
   serverInfo.connection.disconnect();
+  reset(message, serverInfo, queue, client, false);
 };
